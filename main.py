92cbe476ce0119015578043efc58c5d577c5defd
zach <zach@localhost.localdomain>
2026-06-15 13:14:13 -0500

    Implemented step 1: write_last_viewed() and fetch_head() hash functions.

--- main.py
+++ main.py
@@ -8,25 +8,23 @@ def get_last_seen():
     pass
 
 def get_current_head():
-    
+    result = subprocess.run(["git", "rev-parse", "HEAD"], capture_output=True, text=True)
+    return result.stdout
 
         
     
-def init():
+def write_new_last_seen():
     
-   # This will run on every startup to see if a .git directory exists. If it does, it will set the HEAD commit as last_seen.
-   # It will accomplish this by writing a text file "git-summary-state" to the .git directory
-    
+   # This function writes a new last_seen hash to the text file 'git-summary-state' in the .git directory if it exists and exits with code 1 otherwise.
     path = Path.cwd() / ".git"
     if path.is_dir() == False:
         print("Exited with error code: 1")
         sys.exit("Git repository not initialized. Please initialize a git repository via \'git init\' or otherwise run this in a directory with a valid git repository.")
     with (path / 'git-summary-state.txt').open('w') as state:
-        state.write("last_seen: {get_current_head()}")
-    
+        state.write(f"last_seen: {get_current_head()}")
          
 def main():
-    init()
+
 
 if __name__ == "__main__":
     main()
